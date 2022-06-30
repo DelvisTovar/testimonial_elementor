54e5da625bb2c1521f5913e2377a7031ec30c402
Delvis Tovar <trabajoremotodt@gmail.com>
2022-06-30 10:43:32 -0400

    wordpress correcion

--- index.php
+++ index.php
@@ -19,25 +19,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 	require_once('main.php');
 	$redo_object = new WidgetsTestimonialDTMAin();
 
-defined( 'W_DT_BASEPATH' ) || define( 'W_DT_BASEPATH', plugin_dir_path( __FILE__ ) );
-defined( 'W_DT_BASEURI' ) || define( 'W_DT_BASEURI', plugin_dir_url( __FILE__ ) );
+defined( 'TESTIMONIALDT_BASEPATH' ) || define( 'TESTIMONIALDT_BASEPATH', plugin_dir_path( __FILE__ ) );
+defined( 'TESTIMONIALDT_BASEURI' ) || define( 'TESTIMONIALDT_BASEURI', plugin_dir_url( __FILE__ ) );
 
 /** Enqueue plugin scripts */
 add_action( 'wp_enqueue_scripts', function() {
-	wp_enqueue_script( 'bootstrap.min.js',W_DT_BASEURI .'public/js/bootstrap.bundle.min.js', array('jquery'), '5.1.3', true );
-	wp_enqueue_script( 'owl.carousel.min.js', W_DT_BASEURI .'public/js/owl.carousel.min.js', array('jquery'), generarCodigo(3), true );
-	wp_enqueue_script( 'w-dt.js', W_DT_BASEURI . 'public/js/w-dt.js', array('jquery'), generarCodigo(3), true );
+	wp_enqueue_script( 'bootstrap.min.js',TESTIMONIALDT_BASEURI.'public/js/bootstrap.bundle.min.js', array('jquery'), '5.1.3', true );
+	wp_enqueue_script( 'owl.carousel.min.js',TESTIMONIALDT_BASEURI.'public/js/owl.carousel.min.js', array('jquery'), generarCodigo(3), true );
+	wp_enqueue_script( 'w-dt.js',TESTIMONIALDT_BASEURI.'public/js/w-dt.js', array('jquery'), generarCodigo(3), true );
 
 } );
 
 /** Enqueue plugin styles */
 add_action( 'wp_enqueue_scripts', function() {
 	wp_enqueue_style( 'fonts.googleapis.com', 'https://fonts.googleapis.com/css?family=Roboto+Slab:400,700');
-	wp_enqueue_style( 'bootstrap.min.css',  W_DT_BASEURI . 'public/css/bootstrap5.1.3.min.css', array(), '5.1.3');
-	wp_enqueue_style( 'awesome.css',  W_DT_BASEURI . 'public/css/font-awesome.min.css', array(), '4.7.0');
-	wp_enqueue_style( 'owl.carousel.min.css', W_DT_BASEURI . 'public/css/owl.carousel.min.css', array(), generarCodigo(3));
-	wp_enqueue_style( 'animate.min.css', W_DT_BASEURI . 'public/css/animate.min.css', array(), generarCodigo(3));
-	wp_enqueue_style( 'w-dt.css', W_DT_BASEURI . 'public/css/w-dt.css', array(), generarCodigo(3));
+	wp_enqueue_style( 'bootstrap.min.css', TESTIMONIALDT_BASEURI.'public/css/bootstrap5.1.3.min.css', array(), '5.1.3');
+	wp_enqueue_style( 'awesome.css', TESTIMONIALDT_BASEURI.'public/css/font-awesome.min.css', array(), '4.7.0');
+	wp_enqueue_style( 'owl.carousel.min.css',TESTIMONIALDT_BASEURI.'public/css/owl.carousel.min.css', array(), generarCodigo(3));
+	wp_enqueue_style( 'animate.min.css',TESTIMONIALDT_BASEURI.'public/css/animate.min.css', array(), generarCodigo(3));
+	wp_enqueue_style( 'w-dt.css',TESTIMONIALDT_BASEURI.'public/css/w-dt.css', array(), generarCodigo(3));
 
 	
 }, 50);
