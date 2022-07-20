4a365e5c2ba1f11d9d80ec9300f2d45831d2f7e2
Delvis Tovar <trabajoremotodt@gmail.com>
2022-07-20 12:00:32 -0400

    update v1.0.1
    multiple templates for testimonials

--- widgets/widget-testimonial.php
+++ widgets/widget-testimonial.php
@@ -36,6 +36,22 @@ class WidgetsTestimonialDTTestimonial extends Widget_Base {
 	        'label_block' => true,
 	      ]
 	    );
+	    $this->add_control(
+	      'testimonial_view',
+	      [
+	        'label' => esc_html__( 'Template', 'widgetstestimonialdt' ),
+	        'type' => Controls_Manager::SELECT,
+	        'default' => '1',
+	        'options' => [
+	          /*'5' => esc_html__( '5', 'widgetstestimonialdt' ),
+	          '4' => esc_html__( '4', 'widgetstestimonialdt' ),*/
+	          '3' => esc_html__( '3', 'widgetstestimonialdt' ),
+	          '2' => esc_html__( '2', 'widgetstestimonialdt' ),
+	          '1' => esc_html__( '1', 'widgetstestimonialdt' ),
+	        ],
+	        'frontend_available' => true,
+	      ]
+	    );
 		$this->end_controls_section();
 
 		$this->start_controls_section(
@@ -84,7 +100,7 @@ class WidgetsTestimonialDTTestimonial extends Widget_Base {
 	    $repeater_testimonial->add_control(
 	      'testimonial_rating',
 	      [
-	        'label' => esc_html__( 'Estrellas', 'widgetstestimonialdt' ),
+	        'label' => esc_html__( 'Stars', 'widgetstestimonialdt' ),
 	        'type' => Controls_Manager::SELECT,
 	        'default' => '5',
 	        'options' => [
@@ -101,7 +117,7 @@ class WidgetsTestimonialDTTestimonial extends Widget_Base {
 	    $this->add_control(
 	      'testimonial_list',
 	      [
-	        'label' => __( 'Lista testimonios', 'widgetstestimonialdt' ),
+	        'label' => __( 'Testimonials list', 'widgetstestimonialdt' ),
 	        'type' => \Elementor\Controls_Manager::REPEATER,
 	        'fields' => $repeater_testimonial->get_controls(),
 	        'default' => [
@@ -117,11 +133,12 @@ class WidgetsTestimonialDTTestimonial extends Widget_Base {
 		$this->end_controls_section();
 	}
 	protected function render(){
-
 		$settings = $this->get_settings_for_display();
+		$view = $settings['testimonial_view'];
+		#diseño 1
+		if ($view == 1) {
 		?>
 		<!-- HTML DESIGN HERE -->
-		
 		<section>
 			<div class="customer-feedback">
 				<div class="container text-center">
@@ -132,7 +149,6 @@ class WidgetsTestimonialDTTestimonial extends Widget_Base {
 							</div>
 						</div><!-- /End col -->
 					</div><!-- /End row -->
-
 					<div class="row">
 						<div class="col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 cont-test">
 							<?php if ($settings['testimonial_list']) { ?>
@@ -178,7 +194,6 @@ class WidgetsTestimonialDTTestimonial extends Widget_Base {
 								<?php } ?>
 							</div>
 							<!-- /side thumbnail -->
-
 						</div><!-- /End col -->
 					</div><!-- /End row -->
 				</div><!-- /End container -->
@@ -186,5 +201,70 @@ class WidgetsTestimonialDTTestimonial extends Widget_Base {
 		</section>
 		<!-- HTML END DESIGN HERE -->
 		<?php
+		} elseif ($view == 2) { ?>
+		  <div class="testimonials2 text-center">
+		    <div class="container">
+		      <h3><?php echo esc_attr($settings['testimonial_title']); ?></h3>
+		      <div class="row">
+		      	<?php if ($settings['testimonial_list']) { ?>
+		      		<?php foreach ($settings['testimonial_list'] as $item) { ?>
+				        <div class="col-md-6 col-lg-4">
+				          <div class="card border-light bg-light text-center">
+				            <i class="fa fa-quote-left fa-3x card-img-top rounded-circle" aria-hidden="true"></i>
+				            <div class="card-body blockquote">
+				              <p class="card-text"><?php echo esc_attr($item['testimonial_description']); ?></p>
+				              <footer class="blockquote-footer">
+				              	<cite title="Source Title"><?php echo esc_attr($item['testimonial_name']); ?></cite>
+				              </footer>
+				            </div>
+				          </div>
+				        </div>
+		        	<?php } ?>
+		    	<?php } ?>
+		      </div>
+		    </div>
+		  </div>
+		<?php
+		} elseif ($view = 3){ ?>
+			<div class="container testimonial3  text-center">
+	            <div class="heading white-heading"><?php echo esc_attr($settings['testimonial_title']); ?></div>
+				<div id="carouselExampleIndicators" class="carousel slide testimonial3_control_button" data-bs-ride="true">
+				  <div class="carousel-indicators">
+				  	<?php if ($settings['testimonial_list']) { ?>
+				  		<?php $slider3count1 = 0; ?>
+		      			<?php foreach ($settings['testimonial_list'] as $item1) { ?>
+		      				<?php $slider3ClassActive1 = ($slider3count1 == 0) ? 'active' : ''; ?>
+				    		<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="<?php echo $slider3count1; ?>" class="<?php echo $slider3ClassActive1; ?>" aria-current="true" aria-label="Slide"></button>
+				    		<?php $slider3count1++; ?> 
+						<?php } ?>
+					<?php } ?>
+				  </div>
+				  <div class="carousel-inner" id="testimonial3">
+				  	<?php if ($settings['testimonial_list']) { ?>
+				  		<?php $slider3count = 1; ?>
+		      			<?php foreach ($settings['testimonial_list'] as $item) { ?>
+		      				<?php $slider3ClassActive = ($slider3count == 1) ? 'active' : ''; ?>
+						  	<div class="carousel-item <?php echo $slider3ClassActive; ?>">
+						  		<div class="testimonial3_slide">
+						  			<img src="<?php echo esc_attr($item['testimonial_image']['url']); ?>" class="img-circle img-responsive" />
+						  			<p><?php echo esc_attr($item['testimonial_description']); ?></p>
+						  			<h4><?php echo esc_attr($item['testimonial_name']); ?></h4>
+						  		</div>
+						  	</div>
+						  	<?php $slider3count++; ?> 
+						<?php } ?>
+					<?php } ?>
+				  </div>
+				  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				    <span class="visually-hidden">Previous</span>
+				  </button>
+				  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+				    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+				    <span class="visually-hidden">Next</span>
+				  </button>
+				</div>	
+			</div>
+		<?php }
 	}
 }
